c3b6865b7955e3c8350bba2d023b46bf7336e222
Stanislava <ivanenko.stasy@gmail.com>
2023-04-25 15:23:32 +0300

    OOP hw5

--- src/main/java/sem_7/ViewCalculator.java
+++ src/main/java/sem_7/ViewCalculator.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class ViewCalculator {
 
     private ICalculableFactory calculableFactory;
@@ -16,32 +17,32 @@ public class ViewCalculator {
     public void run() {
         while (true) {
             System.out.println("Если вы хотите ввести комплексное число, вводите его в формате a+ib");
-            Integer[] primaryArg = promptIntAr("Введите первый аргумент: ");
+            Integer[] primaryArg = Prompt.promptIntAr("Введите первый аргумент: ");
             Calculable calculator = calculableFactory.create(primaryArg);
             Loggable loggable = new LoggerCalc();
-            loggable.saveLog("Inputed "+massView(primaryArg));
+            loggable.saveLog("Inputed "+Prompt.massView(primaryArg));
             while (true) {
-                String cmd = prompt("Введите команду (*, +, =) : ");
+                String cmd = Prompt.prompt("Введите команду (*, +, =) : ");
                 if (cmd.equals("*")) {
-                    Integer[] arg = promptIntAr("Введите второй аргумент: ");
+                    Integer[] arg = Prompt.promptIntAr("Введите второй аргумент: ");
                     calculator.multi(arg);
-                    loggable.saveLog("Inputed "+massView(arg));
+                    loggable.saveLog("Inputed "+Prompt.massView(arg));
                     continue;
                 }
                 if (cmd.equals("+")) {
-                    Integer[] arg = promptIntAr("Введите второй аргумент: ");
+                    Integer[] arg = Prompt.promptIntAr("Введите второй аргумент: ");
                     calculator.sum(arg);
-                    loggable.saveLog("Inputed "+massView(arg));
+                    loggable.saveLog("Inputed "+Prompt.massView(arg));
                     continue;
                 }
                 if (cmd.equals("=")) {
                     Integer[] result = calculator.getResult();
-                    System.out.printf("Результат %s\n", massView(result));
-                    loggable.saveLog("Resulted "+massView(result));
+                    System.out.printf("Результат %s\n", Prompt.massView(result));
+                    loggable.saveLog("Resulted "+Prompt.massView(result));
                     break;
                 }
             }
-            String cmd = prompt("Еще посчитать (Y/N)?");
+            String cmd = Prompt.prompt("Еще посчитать (Y/N)?");
             if (cmd.equals("Y")) {
                 continue;
             }
@@ -49,56 +50,5 @@ public class ViewCalculator {
         }
     }
 
-    private String prompt(String message) {
-        Scanner in = new Scanner(System.in);
-        System.out.print(message);
-        return in.nextLine();
-    }
 
-    private int promptInt(String message) {
-        Scanner in = new Scanner(System.in);
-        System.out.print(message);
-        return Integer.parseInt(in.nextLine());
-    }
-    public static Integer[] promptIntAr(String message) {
-        Scanner in = new Scanner(System.in);
-        System.out.println(message);
-        String[] string = in.nextLine().split("");
-        Integer[] res_end = new Integer[2];
-        List<Integer> res = new ArrayList<>();
-        if (Character.isDigit(string[0].charAt(0))){
-            Integer toAdd = Integer.parseInt(string[0]);
-            res.add(toAdd);
-        }
-        for (int i = 1; i < string.length; i++) {
-            if (Character.isDigit(string[i].charAt(0))) {
-                if (Character.isDigit(string[i-1].charAt(0))) {
-                    res.set(res.size()-1,
-                            res.get(res.size()-1)*10+Integer.parseInt(string[i]));
-                } else {
-                    Integer toAdd = Integer.parseInt(string[i]);
-                    res.add(toAdd);
-                }
-            }
-        }
-        if (res.size()>2){
-            System.out.println("Это слишком даже для комплексного числа(уменьште количество чисел)");
-            promptIntAr(message);
-        } else if (res.size()==1) {
-            res_end[0]=res.get(0);
-            res_end[1]=0;
-        } else if (res.size()<1) {
-            res_end[0] = 0;
-            res_end[1] = 0;
-        } else if (res.size()==2) {
-            res_end[0]=res.get(0);
-            res_end[1]=res.get(1);
-        }
-        return res_end;
-    }
-    private String massView(Integer[] arg){
-        return Arrays.toString(arg)
-                .substring(1, 3*arg.length)
-                .replaceAll(", ", "+i");
-    }
 }
